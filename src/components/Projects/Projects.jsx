888309a3db9902f8c9348af48914f9b1dfcd805e
Jhamil Fernandez <j-fernandez95@hotmail.com>
2023-03-29 18:42:22 -0500

    Adding e-comerce to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,7 @@
 import rick from "../../img/rm.jpg";
 import pokemon from "../../img/pokemon.jpg";
+import store from "../../img/storeClient.png"
+import customer from "../../img/customer.png"
 import style from "./Projects.module.css";
 import ReactGA from "react-ga";
 
@@ -56,6 +58,51 @@ const Projects = () => {
               Repo
             </a>
           </div>
+          
+        </div>
+        <div>
+          <div className={style.box}>
+            Store App
+            <a
+              href="https://storespacefood.netlify.app/"
+              target="_blank"
+              onClick={() => clickHandler("Store App")}
+              onAuxClick={() => clickHandler("Store App")}
+            >
+              <img src={store} alt="Store APP" className={style.img} />
+            </a>
+            <a
+              href="https://github.com/jfernandez141/storeclient"
+              target="_blank"
+              onClick={() => clickHandler("Store Repo")}
+              onAuxClick={() => clickHandler("Store App")}
+            >
+              Repo
+            </a>
+          </div>
+          
+        </div>
+        <div>
+          <div className={style.box}>
+            Customer App
+            <a
+              href="https://spacefood.netlify.app/"
+              target="_blank"
+              onClick={() => clickHandler("Customer App")}
+              onAuxClick={() => clickHandler("Customer App")}
+            >
+              <img src={customer} alt="Customer App" className={style.img} />
+            </a>
+            <a
+              href="https://github.com/jfernandez141/customerclient"
+              target="_blank"
+              onClick={() => clickHandler("Customer Repo")}
+              onAuxClick={() => clickHandler("Customer App")}
+            >
+              Repo
+            </a>
+          </div>
+          
         </div>
       </div>
     </div>
