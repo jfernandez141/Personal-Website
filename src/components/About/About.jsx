b352de0c420ceec4cc97b9edf84dd5621083d2b6
Jhamil Fernandez <j-fernandez95@hotmail.com>
2023-05-17 16:06:11 -0500

    About improvements

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,17 +14,7 @@ const About = () => {
       <h2 className={style.aboutTittle}>About Me</h2>
       <div className={style.section}>
         <p className={style.about}>
-          Full Stack web developer with a background in finance and data
-          analysis, with extensive experience developing ecommerce full stack
-          web pages, with administrative management and payment gateways, among
-          other functions, handling of programming languages such as JavaScript
-          and Python, as well as in the management of PostgreSQL databases and
-          Frontend and Backend technologies such as React, Redux, NodeJS,
-          Express, Sequelize and Axios. I have advanced skills in development
-          tools like Visual Studio Code and agile Scrum methodologies. In
-          addition, I have an advanced level of English (B2) and I consider
-          myself a committed and dedicated person, goal-oriented and capable of
-          leading and working as a team to achieve optimal results.
+        Full Stack web developer with a background in finance and data analysis, focused on backend developer, with extensive experience developing APIs and full stack web pages, with administrative management and payment gateways, among other functions, handling of programming languages such as JavaScript and Python, as well as in the management of PostgreSQL databases and Frontend and Backend technologies such as React, Redux, NodeJS, Express, Sequelize among others. I have advanced skills in development tools like Visual Studio Code and agile Scrum methodologies. In addition, I have an advanced level of English (B2) and I consider myself a committed and dedicated person, goal-oriented and capable of leading and working as a team to achieve optimal results.
         </p>
       </div>
     </div>
