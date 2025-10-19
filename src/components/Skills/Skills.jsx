3ac29f0d07e989ff9309f07e2f725e9deed70665
jfernandez141 <j-fernandez95@hotmail.com>
2025-10-18 23:04:32 -0500

    Refactor Skills component: reorganize skills layout into categories and enhance styles for improved readability and visual appeal

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,72 +1,73 @@
 import js from "../../img/js.png";
-import git from "../../img/git.png";
-import reactjs from "../../img/reactjs.png";
 import tp from "../../img/typescript.png";
-import sql from "../../img/sql.png";
-import postgres from "../../img/postgresql.png";
+import python from "../../img/python.png";
 import nodejs from "../../img/nodejs.png";
 import express from "../../img/express.png";
-import sequelize from "../../img/sequelize.png";
-import aws from "../../img/aws.png";
-import python from "../../img/python.png";
-import docker from "../../img/docker.png"
+import nestjs from "../../img/nestjs.png";
+import django from "../../img/django.png";
+import postgres from "../../img/postgresql.png";
+import mysql from "../../img/mysql.png";
+import mongo from "../../img/mongo.png";
+import docker from "../../img/docker.png";
+import k8s from "../../img/kubernetes.png";
+import gcp from "../../img/gcp.png";
+import vercel from "../../img/vercel.png";
+import git from "../../img/git.png";
+import linux from "../../img/linux.png";
 import style from "./Skills.module.css";
 
 const Skills = () => {
   return (
     <div id="skills" className={style.skillsBody}>
       <div className={style.container}>
-      <h2 className={style.tittle}>Tech Skills</h2>
-        <div className={style.spanContainer}>          
-          <span className={style.span}>
-            <img src={js} alt="JavaScript" className={style.img} />
-            JavaScript
-          </span>
-          <span className={style.span}>
-            <img src={tp} alt="typescript" className={style.img} />
-            Typescript
-          </span>
-          <span className={style.span}>
-            <img src={nodejs} alt="nodejs" className={style.img} />
-            Node.js
-          </span>
-          <span className={style.span}>
-            <img src={docker} alt="Docker" className={style.img} />
-            Docker
-          </span>
-          <span className={style.span}>
-            <img src={reactjs} alt="ReactJs" className={style.img} />
-            React.js
-          </span>          
-          <span className={style.span}>
-            <img src={express} alt="express" className={style.img} />
-            Express.JS
-          </span>
-          <span className={style.span}>
-            <img src={sequelize} alt="sequelize" className={style.img} />
-            Sequelize
-          </span>
-          <span className={style.span}>
-            <img src={postgres} alt="postgres" className={style.img} />
-            PostgreSQL
-          </span>
-          <span className={style.span}>
-            <img src={sql} alt="sql" className={style.img} />
-            SQL
-          </span>
-          <span className={style.span}>
-            <img src={python} alt="sql" className={style.img} />
-            Python
-          </span>
-          <span className={style.span}>
-            <img src={aws} alt="sql" className={style.img} />
-            AWS
-          </span>
-          <span className={style.span}>
-            <img src={git} alt="git" className={style.img} />
-            Git
-          </span>
+        <h2 className={style.tittle}>Tech Skills</h2>
+
+        {/* Lenguajes */}
+        <h3 className={style.category}>Languages</h3>
+        <div className={style.spanContainer}>
+          <span className={style.span}><img src={js} alt="JavaScript" className={style.img} />JavaScript</span>
+          <span className={style.span}><img src={tp} alt="TypeScript" className={style.img} />TypeScript</span>
+          <span className={style.span}><img src={python} alt="Python" className={style.img} />Python</span>
         </div>
+
+        {/* Frameworks */}
+        <h3 className={style.category}>Frameworks</h3>
+        <div className={style.spanContainer}>
+          <span className={style.span}><img src={nodejs} alt="Node.js" className={style.img} />Node.js</span>
+          <span className={style.span}><img src={express} alt="Express" className={style.img} />Express.js</span>
+          <span className={style.span}><img src={nestjs} alt="NestJS" className={style.img} />NestJS</span>
+          <span className={style.span}><img src={django} alt="Django" className={style.img} />Django</span>
+        </div>
+
+        {/* Bases de datos */}
+        <h3 className={style.category}>Databases</h3>
+        <div className={style.spanContainer}>
+          <span className={style.span}><img src={postgres} alt="PostgreSQL" className={style.img} />PostgreSQL</span>
+          <span className={style.span}><img src={mysql} alt="MySQL" className={style.img} />MySQL</span>
+          <span className={style.span}><img src={mongo} alt="MongoDB" className={style.img} />MongoDB</span>
+        </div>
+
+        {/* DevOps */}
+        <h3 className={style.category}>DevOps & Cloud</h3>
+        <div className={style.spanContainer}>
+          <span className={style.span}><img src={docker} alt="Docker" className={style.img} />Docker</span>
+          <span className={style.span}><img src={k8s} alt="Kubernetes" className={style.img} />Kubernetes</span>
+          <span className={style.span}><img src={gcp} alt="GCP" className={style.img} />GCP</span>
+          <span className={style.span}><img src={vercel} alt="Vercel" className={style.img} />Vercel</span>
+        </div>
+
+        {/* Otros */}
+        <h3 className={style.category}>Others</h3>
+        <div className={style.spanContainer}>
+          <span className={style.span}><img src={git} alt="Git" className={style.img} />Git</span>
+          <span className={style.span}><img src={linux} alt="Linux" className={style.img} />Linux</span>
+          <span className={style.span}>CI/CD</span>
+          <span className={style.span}>REST / GraphQL</span>
+          <span className={style.span}>RabbitMQ</span>
+          <span className={style.span}>Microservices</span>
+          <span className={style.span}>Clean Architecture</span>
+        </div>
+
       </div>
     </div>
   );
